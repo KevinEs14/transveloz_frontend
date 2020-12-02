a00773f86c622f65c204ddaf20541ba35925b289
Joel Quispe <joel.alejandro.quispe.cussi@gmail.com>
2020-12-02 16:58:40 -0400

    design form of vehicle register

--- lib/pages/Vehicle/VehicleRegisterPage.dart
+++ lib/pages/Vehicle/VehicleRegisterPage.dart
@@ -22,9 +22,8 @@ class _VehicleRegister extends State<VehicleRegister>{
   TextEditingController vehicleBrandText = TextEditingController();
   TextEditingController vehicleBrandModel = TextEditingController();
 
-  var _items = ['Camioneta', 'Volqueta', 'Minibus'];
-  String _vista = "Seleccione una opcion";
-
+  var _itemsTypeVehicle = ['Camioneta', 'Volqueta', 'Minibus'];
+  String _nameTypeVehicle = "Seleccione una opcion";
 
 
 
@@ -55,10 +54,142 @@ class _VehicleRegister extends State<VehicleRegister>{
                 ),
                 child: Column(
                     children: <Widget>[
+                      SizedBox(
+                        height: 20,
+                      ),
                       Container(
+
                         margin: EdgeInsets.only(top: size.height*0.04),
                         child: Center(
                           child: Text("AÑADIR NUEVO VEHICULO",style: TextStyle(color: Colors.white,fontSize: size.height*0.035,fontWeight: FontWeight.bold),),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        width: size.width*0.82,
+                        padding: EdgeInsets.all(1),
+                        child: TextField(
+                          decoration:InputDecoration(
+                            fillColor: Color(0xFFEEEBD3),
+                            filled: true,
+                            prefixText: "    ",
+                            border: InputBorder.none,
+                            hintText: "Ingrese numero de placa",
+                            hintStyle: TextStyle(color: Color(0xFF0F4C5C)),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                                borderSide: BorderSide(
+                                    color: Color(0xFF0F4C5C),
+                                    width: 3
+                                )
+                            ),
+
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        width: size.width*0.82,
+                        padding: EdgeInsets.all(1),
+                        child: TextFormField(
+                          decoration:InputDecoration(
+                            fillColor: Color(0xFFEEEBD3),
+                            filled: true,
+                            prefixText: "    ",
+                            border: InputBorder.none,
+                            hintText: "Ingrese la capacidad del vehiculo",
+                            hintStyle: TextStyle(color: Color(0xFF0F4C5C)),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                                borderSide: BorderSide(
+                                    color: Color(0xFF0F4C5C),
+                                    width: 3
+                                )
+                            ),
+
+                          ),
+                        ),
+                      ),
+
+
+                      SizedBox(
+                        height: 10,
+                      ),
+
+
+                      Container(
+                        width: size.width*0.82,
+                        padding: EdgeInsets.only(left:16,right: 16),
+                        decoration: BoxDecoration(
+                          color: Color(0xFFEEEBD3),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+
+
+
+                            border: Border.all(
+                                color: Color(0xFF0F4C5C),
+                                width: 3,
+
+                            ),
+
+
+                        ),
+
+
+                        child: DropdownButton(
+                          hint: Text(_nameTypeVehicle, style: TextStyle(color: Color(0xFF0F4C5C))),
+                          dropdownColor: color4,
+                          elevation: 5,
+                          icon: Icon(Icons.arrow_drop_down),
+                          iconSize: 36.0,
+                          isExpanded: true,
+                          iconDisabledColor: color4,
+
+                          iconEnabledColor: Color(0xFF0F4C5C),
+                          onChanged: (newValue){
+                            setState(() {
+                              _nameTypeVehicle = newValue;
+                            });
+                          },
+                          items: _itemsTypeVehicle.map((newValue){
+                            return DropdownMenuItem(
+                                value: newValue,
+                                child: Text(newValue, style: TextStyle(color: color1)));
+                          }).toList(),
+                        ),
+                      ),
+
+                      SizedBox(
+                        height: 10,
+                      ),
+
+
+
+                      Container(
+                        width: size.width*0.82,
+                        padding: EdgeInsets.all(1),
+                        child: TextFormField(
+                          decoration:InputDecoration(
+                            fillColor: Color(0xFFEEEBD3),
+                            filled: true,
+                            prefixText: "    ",
+                            border: InputBorder.none,
+                            hintText: "Ingrese la marca del vehiculo",
+                            hintStyle: TextStyle(color: Color(0xFF0F4C5C)),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                                borderSide: BorderSide(
+                                    color: Color(0xFF0F4C5C),
+                                    width: 3
+                                )
+                            ),
+
+                          ),
                         ),
                       ),
                       SizedBox(
@@ -67,32 +198,44 @@ class _VehicleRegister extends State<VehicleRegister>{
                       Container(
                         width: size.width*0.82,
                         padding: EdgeInsets.all(1),
-                        child: TextField(
-                          decoration: InputDecoration(
+                        child: TextFormField(
+                          decoration:InputDecoration(
                             fillColor: Color(0xFFEEEBD3),
                             filled: true,
                             prefixText: "    ",
+
                             border: InputBorder.none,
-                            hintText: "Ingrese numero de placa",
+                            hintText: "Ingrese el modelo del vehiculo",
                             hintStyle: TextStyle(color: Color(0xFF0F4C5C)),
                             enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(24)),
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
                                 borderSide: BorderSide(
                                     color: Color(0xFF0F4C5C),
-                                    width: 5
+                                    width: 3
                                 )
                             ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(24)),
-                            ),
+
                           ),
-                          controller: vehicleLicensePlateText,
                         ),
                       ),
-
-
-
-
+                      SizedBox(height: 50),
+                      GestureDetector(
+                        onTap: (){
+                          //user  = Submit();
+                          //if(user != null){
+                          //  Navigator.push(context, MaterialPageRoute(builder: (conext)=>UserRegisterAddress(user)));
+                          //}
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(left: size.width*0.2,right: size.width*0.2),
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            color: Color(0xFF0F4C5C),
+                          ),
+                          child: Center(child: Text("Siguiente   ➜",style: TextStyle(fontSize:size.width*0.058,color: Colors.white,fontWeight: FontWeight.bold),)),
+                        ),
+                      ),
                     ]
 
                 ),
--- lib/repository/url.dart
+++ lib/repository/url.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 var directionUrl="http://10.0.2.2:8070/";
+
+//var directionUrl="http://192.168.1.12:8070/";
